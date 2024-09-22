5bf779c66d9058ee4eeabbc9f359e883e724f36f
Elana Elman <elana.elman@gmail.com>
2024-09-22 13:57:13 -0400

    add draw loop

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // JavaScript source code
+
 const canvas = document.getElementById("catCanvas");
 const ctx = canvas.getContext("2d");
 
-window.addEventListener("load", draw);
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "blue";
+    ctx.fillRect(10, 10, 580, 380);
+}
+
+function startGame() {
+    setInterval(draw, 10); //redraw every 10 seconds
+}
+
+window.addEventListener("load", startGame);
+
+
