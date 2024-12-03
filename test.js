811845cdb7c70de89cc1b679fd467437bcbe4e26
Elana Elman <elana.elman@gmail.com>
2024-12-03 12:52:30 -0500

    Removed Apmt.js

--- test.js
+++ test.js
@@ -1,3 +1,7 @@
+//Small test case. Shows that interrupt works.
+//This runs in test_js.html
+
+
 let hungry = new Cat("Hungry");
 let eat = new Action("Eat", 1, 1, true, 1000, "Food");
 hungry.possibleTasks.push(eat);
@@ -12,20 +16,7 @@ let manager = new Manager(cats, stations, 0);
 
 let distract = new Event(0, "Distraction", undefined)
 
-// time = window.performance.now()
 
-// function run(timeOfAnimationFrame) {
-// 	//Queue main again for the next screen refresh
-// 	//requestAnimationFrame passes a timestamp to main.
-// 	animationFrame = window.requestAnimationFrame(run);
-
-// 	deltaTime = timeOfAnimationFrame - time;
-// 	manager.onUpdate(deltaTime);
-// 	time = timeOfAnimationFrame;
-
-// }
-
-// run(time);
 
 manager.onUpdate(1);
 manager.onUpdate(1);
